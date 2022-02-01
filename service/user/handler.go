2d7a24a14a297f5f52de71c91e02c29a94c73619
Gustavo Cioccari <gustavocioccari@gmail.com>
2022-02-01 10:47:33 -0300

    feat: add user service

---
+++ service/user/handler.go
@@ -0,0 +1,52 @@
+package user
+
+import (
+	"time"
+
+	"github.com/gustavocioccari/go-user-microservice/models"
+	"github.com/gustavocioccari/go-user-microservice/repositories/mongodb/user"
+	uuid "github.com/satori/go.uuid"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type service struct {
+	userRepository user.UserRepository
+}
+
+type UserService interface {
+	Create(user *models.User) (*models.User, error)
+}
+
+func NewUserService(
+	userRepository user.UserRepository,
+) UserService {
+	return &service{
+		userRepository: userRepository,
+	}
+}
+
+func (s *service) Create(user *models.User) (*models.User, error) {
+	user.ID = uuid.NewV4().String()
+	user.CreatedAt, user.UpdatedAt = time.Now(), time.Now()
+
+	hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, err
+	}
+
+	user.Password = string(hash)
+
+	userData, err := s.userRepository.Create(user)
+	if err != nil {
+		return nil, err
+	}
+
+	userCreated, err := s.userRepository.FindById(userData.InsertedID.(string))
+	if err != nil {
+		return nil, err
+	}
+
+	userCreated.Password = ""
+
+	return userCreated, nil
+}
